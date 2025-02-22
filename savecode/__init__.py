24dfb08c71456b758c71e08979f66df864bbf7d6
Austin Wagner <austinwagner@msn.com>
2025-02-22 03:19:34 -0800

    updated to 1.2.9

--- savecode/__init__.py
+++ savecode/__init__.py
@@ -2,7 +2,7 @@
 savecode/__init__.py - Initialize the savecode package with centralized version management.
 """
 
-__version__ = "1.2.8"
+__version__ = "1.2.9"
 
 # Optionally expose plugin manager functions
 from .plugin_manager import run_plugins, list_plugins
