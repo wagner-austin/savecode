0043a895c1bf9a992bcfc66e426d5fe4bfd1b86b
Austin Wagner <austinwagner@msn.com>
2025-02-22 02:06:47 -0800

    Standardize output path handling by always converting to an absolute path

--- savecode/utils/output_manager.py
+++ savecode/utils/output_manager.py
@@ -8,13 +8,11 @@ def configure_output_path(output_arg):
     """
     Configures the output file path.
 
-    If the output_arg is the default ("./temp.txt"), this function returns the absolute path
-    to "temp.txt" in the current working directory. Otherwise, it returns the output_arg as provided.
+    Always converts the provided output path to an absolute path relative to the current working directory.
+    This ensures that all output paths are handled consistently, whether they are provided as relative
+    or absolute paths.
 
     :param output_arg: The output file path argument.
-    :return: The configured output file path.
+    :return: The absolute output file path.
     """
-    if output_arg == "./temp.txt":
-        return os.path.join(os.getcwd(), "temp.txt")
-    else:
-        return output_arg
+    return os.path.abspath(output_arg)
