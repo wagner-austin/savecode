2014549e478cd55773cf19220182bd3a99902123
Austin Wagner <austinwagner@msn.com>
2025-02-22 02:56:05 -0800

    Updated version to 1.2.8

--- savecode/__init__.py
+++ savecode/__init__.py
@@ -2,7 +2,7 @@
 savecode/__init__.py - Initialize the savecode package with centralized version management.
 """
 
-__version__ = "1.2.7"
+__version__ = "1.2.8"
 
 # Optionally expose manager functions
 from .manager import run_plugins, list_plugins
