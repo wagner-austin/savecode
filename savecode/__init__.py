de971a6923c33eb9ae4f9d98bfcc4fc4f951ad63
wagner-austin <austinwagner@msn.com>
2025-03-27 18:06:52 -0700

    Update version to 2.0.1 for added release mechanism, release.ps1

--- savecode/__init__.py
+++ savecode/__init__.py
@@ -2,7 +2,7 @@
 savecode/__init__.py - Initializes the savecode package with version information.
 """
 
-__version__ = "2.0.0"
+__version__ = "2.0.1"
 
 # Optionally expose plugin manager functions.
 from .plugin_manager import run_plugins, list_plugins
